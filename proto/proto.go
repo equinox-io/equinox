74c3706d8e49b5c0bb31425a34d7051a5908bab5
Kyle Conroy <kyle@kyleconroy.com>
2015-09-22 23:12:32 -0700

    Remove incorrect JSON struct tag

--- proto/proto.go
+++ proto/proto.go
@@ -30,7 +30,7 @@ type Response struct {
 	DownloadURL string    `json:"download_url"`
 	Checksum    string    `json:"checksum"`
 	Signature   string    `json:"signature"`
-	Patch       PatchKind `json:"patch_type,string"`
+	Patch       PatchKind `json:"patch_type"`
 	Version     string    `json:"version"`
 	Release     Release   `json:"release"`
 }
